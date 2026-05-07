8a725d704f53c5b43e48c13ae2aa15a365ec459b
alikia2x <alikia2x@outlook.com>
2026-05-07 23:02:30 +0800

    feat: 页面性能遥测

--- src/main.tsx
+++ src/main.tsx
@@ -5,18 +5,36 @@ import App from "./App.tsx";
 import { BrowserRouter, Routes, Route } from "react-router";
 import { Result } from "./pages/Result.tsx";
 import { Toaster } from "@/components/ui/sonner";
+import { onFCP, onLCP, onTTFB, type Metric } from 'web-vitals';
+import { report } from "./lib/telemetry.ts";
+
+function sendToAnalytics(metric: Metric) {
+	const body = {
+		name: metric.name,
+		value: metric.value,
+		id: metric.id,
+	};
+
+	console.log(body);
+
+	report("performance_report", body)
+}
+
+onTTFB(sendToAnalytics);
+onFCP(sendToAnalytics);
+onLCP(sendToAnalytics);
 
 const isDev = import.meta.env.MODE === "development";
 
 createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-        <Toaster />
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<App />} />
-                <Route path="/dev" element={<App />} />
-                {isDev && <Route path="/result/:character" element={<Result />} />}
-            </Routes>
-        </BrowserRouter>
-    </StrictMode>
+	<StrictMode>
+		<Toaster />
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<App />} />
+				<Route path="/dev" element={<App />} />
+				{isDev && <Route path="/result/:character" element={<Result />} />}
+			</Routes>
+		</BrowserRouter>
+	</StrictMode>
 );
